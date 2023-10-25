4fa70f375676fd365d2f75b5bd64097e53924017
Rokibul Hasan <rokibulhasancse@gmail.com>
2023-10-25 17:19:14 +0600

    soap api ui update and code refactor

---
+++ lib/views/promo_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:peanut/controllers/promo_controller.dart';
+import 'package:peanut/views/promo_item_card.dart';
+
+class PromoView extends GetView<PromoController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Promotional Items'),
+      ),
+      body: Obx(
+            () {
+          if (controller.promoItems.isEmpty) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            return ListView.builder(
+              itemCount: controller.promoItems.length,
+              itemBuilder: (context, index) {
+                final promoKey = controller.promoItems[index]['promoKey'];
+                return PromoItemCard(promoKey: promoKey, index: index);
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+
+
+
